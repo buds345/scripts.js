f36279c0223b87c09cbcdd3522c545ef9c935cdb
buds345 <banelebanele938@gmail.com>
2025-01-09 16:05:00 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// Slider Functionality
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Change slide every 3 seconds
+showSlide(currentSlide);
